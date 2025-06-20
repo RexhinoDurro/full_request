ea6930ce5c4768d9bdafc15161b7d4970d1c11dd
RexhinoDurro <rexhinodurro23@gmail.com>
2025-06-20 15:50:40 +0200

    test

--- server/formsite_project/settings.py
+++ server/formsite_project/settings.py
@@ -128,9 +128,11 @@ SIMPLE_JWT = {
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # React client dev
-    "http://localhost:5173",  # Vite dev server
+    "http://localhost:5173",
+    "http://localhost:5174",# Vite dev server
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "http://127.0.0.1:5174",
     "http://localhost:3001",  # Admin dashboard dev
     "http://127.0.0.1:3001",
     # Add your production domains here:
